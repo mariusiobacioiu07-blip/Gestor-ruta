0066b39d9ee1c7383693e03dcf286108db6f0aa9
mariusiobacioiu07-blip <mariusiobacioiu07@gmail.com>
2026-06-08 08:05:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Cambia este número cada vez que subas una actualización a Netlify
 // Eso fuerza al encargado a recibir la versión nueva automáticamente
-const CACHE = 'gestorrutas-v10';
+const CACHE = 'gestorrutas-v11';
 const ASSETS = ['/'];
 
 self.addEventListener('install', e => {
